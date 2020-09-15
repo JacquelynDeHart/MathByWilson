c37269a6b7536ebb005edfd6a342bf5ca0c9010d
Jacquelyn <jacquelyn0890@gmail.com>
2020-09-15 18:10:00 -0500

    Register.kt now pushes to firebase

--- app/app/src/main/java/com/example/mathapp/LoginActivity.kt
+++ app/app/src/main/java/com/example/mathapp/LoginActivity.kt
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
 
         register.setOnClickListener {
-            val intent = Intent(this, Register::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, Register::class.java))
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            finish()
         }
         val user: Editable? = username.text
         val pass: Editable? = password.text
@@ -23,5 +27,16 @@ class LoginActivity : AppCompatActivity() {
         sign_in.setOnClickListener {
             Toast.makeText(this, "Function coming soon" , Toast.LENGTH_SHORT).show()
         }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    fun updateUI(currentUser: FirebaseUser?){
+
     }
 }
