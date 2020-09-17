4b383321b5ff6e13ff82e86fd74695d4e4d61920
ambrosaur <49253746+ambrosaur@users.noreply.github.com>
2020-09-16 21:34:59 -0500

    android youtube player added

--- app/app/src/main/java/com/example/mathapp/Testing.kt
+++ app/app/src/main/java/com/example/mathapp/Testing.kt
@@ -1,11 +1,19 @@
 package com.example.mathapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_testing.*
 
 class Testing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_testing)
+
+        video_button.setOnClickListener{
+            val intent = Intent(this, VideoActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
     }
 }
