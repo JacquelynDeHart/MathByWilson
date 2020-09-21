345910c8cdba28e354a2e1f3779e77fdc60156bc
Jacquelyn <jacquelyn0890@gmail.com>
2020-09-21 14:37:41 -0500

    edited colors resource file. added toolbar.

--- app/app/src/main/java/com/example/mathapp/Testing.kt
+++ app/app/src/main/java/com/example/mathapp/Testing.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_testing.*
 
 
@@ -12,13 +16,47 @@ class Testing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_testing)
+        setSupportActionBar(findViewById(R.id.tbar))
 
 
         video_button.setOnClickListener{
-            val intent = Intent(this, VideoActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, VideoActivity::class.java))
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         }
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+//        return when (item.itemId) {
+//            R.id.action_settings -> true
+//            else -> super.onOptionsItemSelected(item)
+//        }
+        when (item.itemId) {
+            R.id.action_settings -> {
+//                val intent = Intent(this, SettingsMain::class.java)
+//                startActivity(intent)
+//                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+
+            }
+            R.id.action_logout ->{
+                startActivity(Intent(this, LoginActivity::class.java))
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            }
+            R.id.action_about ->{
+                Toast.makeText(this, "An application for mathbywilson.com", Toast.LENGTH_LONG).show()
+            }
+            R.id.action_email ->{
+                Toast.makeText(this, "send email to professor. function coming soon", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
