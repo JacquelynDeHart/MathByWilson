eac730e392048715a1d753a601d8d10335e44bf7
Jacquelyn <40440985+JacquelynJohnson@users.noreply.github.com>
2020-10-02 10:23:33 -0500

    modified Register.kt to push collected data to connected firebase database

--- app/app/src/main/java/com/example/mathapp/Register.kt
+++ app/app/src/main/java/com/example/mathapp/Register.kt
@@ -4,20 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
 
 class Register : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().getReference("Users")
+
 
         add_user.setOnClickListener {
             //push stuff to the firebase database after validating input
@@ -81,6 +85,7 @@ class Register : AppCompatActivity() {
                                 finish()
                             }
                         }
+                    addToDatabase(new_user.text.toString(), first_name.text.toString(), last_name.text.toString())
 
                 } else {
                     // If sign in fails, display a message to the user.
@@ -89,6 +94,15 @@ class Register : AppCompatActivity() {
                         Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    private fun addToDatabase(email: String, fName: String, lName: String) {
+        val user = User(email, fName, lName)
+
+        val ref: String? = database.push().key
+        database.child(ref.toString()).setValue(user).addOnCompleteListener{
+            Toast.makeText(applicationContext, "User saved to database successfully", Toast.LENGTH_LONG).show()
+        }
     }
 
 
